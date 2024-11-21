c1932fd30f99aab4740e04b22190cd15d2d0fb99
frankosorioweb <58038467+frankosorioweb@users.noreply.github.com>
2024-11-20 23:28:31 -0300

    First commit

---
+++ src/main/java/py/com/metropolitano/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package py.com.metropolitano;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author frank
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
